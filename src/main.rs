f62a5145a0135a623f238c7b488416f189887212
James Ding <jamesding365@gmail.com>
2025-10-14 13:49:34 -0500

    feat: implement basic Axum server with a ping route

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/ping", get(ping));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    println!("OGIS server listening on http://0.0.0.0:3000");
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn ping() -> &'static str {
+    "pong"
 }
