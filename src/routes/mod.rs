67f065ce0d804ffd4d076abfa10fff8fbd0642a1
James Ding <jamesding365@gmail.com>
2025-11-02 20:37:17 -0600

    feat: refactor router setup to streamline route definitions and middleware application

--- src/routes/mod.rs
+++ src/routes/mod.rs
@@ -9,21 +9,15 @@ use utoipa::OpenApi;
 use utoipa_swagger_ui::SwaggerUi;
 
 pub fn create_router(state: AppState) -> Router {
-    let protected_routes =
-        Router::new()
-            .route("/", get(index::generate))
-            .layer(middleware::from_fn_with_state(
-                state.clone(),
-                hmac_auth_middleware,
-            ));
-
-    let public_routes = Router::new().route("/health", get(health::health_check));
     let swagger_ui = SwaggerUi::new("/docs").url("/api-docs/openapi.json", docs::ApiDoc::openapi());
 
-    // Combine routes
     Router::new()
-        .merge(protected_routes)
-        .merge(public_routes)
-        .with_state(state)
+        .route("/", get(index::generate))
+        .route_layer(middleware::from_fn_with_state(
+            state.clone(),
+            hmac_auth_middleware,
+        ))
+        .route("/health", get(health::health_check))
         .merge(swagger_ui)
+        .with_state(state)
 }
